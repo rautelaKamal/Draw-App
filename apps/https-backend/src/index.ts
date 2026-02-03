cb4677011a0729d5f979faa4b8c385fae2ee17a1
Your Name <your.email@example.com>
2026-02-03 23:46:05 +0530

    new db database

--- apps/https-backend/src/index.ts
+++ apps/https-backend/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from '@repo/backend-common';
 import { middleware } from "./middleware";
-import {CreateUserSchema} from "@repo/common/types"
+import {CreateUserSchema,CreateRoomSchema,SigninSchema} from "@repo/common/types"
 const app = express();
 
 
@@ -22,6 +22,13 @@ app.post("/signup",(req,res)=>{
 })
 
 app.post("/signin",(req,res)=>{
+   const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+      return res.json({
+        message: "incorrect inputs"
+      })
+      return;
+    }
   const userId = 1;
   const token = jwt.sign({
     userId 
@@ -33,6 +40,13 @@ app.post("/signin",(req,res)=>{
 })
 
 app.post("/room ",middleware,(req,res)=>{
+   const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+      return res.json({
+        message: "incorrect inputs"
+      })
+      return;
+    }
   // db call 
   res.json({
     roomId:123
